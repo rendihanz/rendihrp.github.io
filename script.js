266b0eb24a2e2b52fc1683fe0d8421354213206c
rendihanz <160507180+rendihanz@users.noreply.github.com>
2024-02-19 22:01:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const audio =
+
+docamentbqdyrySdbeament.bodyg');
+
+audio.addEventListener('play', () => { body.classList.add('play-music'); });
+
+audio.addEventListener('pause', () => { body.classList.remove('play-music'); });
